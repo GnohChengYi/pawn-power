4311d4776bb4a52ba92ecd8400c32216de55aeae
GnohChengYi <chengyignoh@gmail.com>
2024-10-26 19:03:24 +0800

    use different version code for new release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.pawnpower"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
+        versionCode = 3
         versionName = "0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
